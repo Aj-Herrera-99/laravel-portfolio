b29507c154d5d26f97f864078e2dd084e2e7ba21
Aj-Herrera-99 <ajherrera999@gmail.com>
2025-04-03 23:20:58 +0200

    digging up infos

---
+++ resources/views/admin-control.blade.php
@@ -0,0 +1,12 @@
+<x-app-layout>
+
+    <p class="text-gray-900 dark:text-gray-100 max-w-7xl mx-auto py-12">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Omnis, earum
+        facilis quo explicabo tenetur ipsa quos
+        hic dolore iusto aut similique eius. Omnis facere minus itaque magni accusamus repudiandae delectus consequatur
+        dicta! Molestias voluptatibus iure vero accusantium praesentium ipsum et, tempore pariatur, nesciunt earum
+        ducimus laboriosam autem molestiae? Minima nulla harum fugiat ducimus magni qui consectetur architecto eos, nisi
+        iste voluptate modi nesciunt nihil cupiditate nostrum corporis laborum repudiandae ea impedit. Sit optio vel
+        inventore voluptates velit magnam nisi. Sint pariatur, error aut aspernatur neque quis, ex consequatur delectus
+        in quia similique illum, fuga dignissimos. Porro maxime eveniet natus veniam.</p>
+
+</x-app-layout>
